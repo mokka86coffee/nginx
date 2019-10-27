da9552246a1599a80671f3484d40cfee6fcb6133
Evgeniy_Marushev <emarushev@eshe.studio>
2019-10-27 03:48:43 +0300

    init

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3020;
 
 app.get('/', (req, res) => {
-    res.send('<p>1111</p>');
+    res.send('<p>11111</p>');
 });
 
 app.listen(PORT, () => console.log(`express is started on ${PORT}`));
